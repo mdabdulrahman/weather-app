e7729492f4547b07d3524cd56b1a0589902df9f6
Abdul Rahman <abdulrahmanasus@outlook.com>
2023-06-30 17:31:53 +0530

    responsive check 2

--- src/Analytics.jsx
+++ src/Analytics.jsx
@@ -91,7 +91,7 @@ function Analytics() {
 
 <div className='flex items-center '>
 <svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 0 24 24" width="26px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M6 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm12 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm-6 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/></svg>
-<img className='ml-6 bg-[#CBE2FF] p-1 rounded-lg w-12' src={profile}/>
+<img className='ml-6 hidden lg:block bg-[#CBE2FF] p-1 rounded-lg w-12' src={profile}/>
 </div>
 </div>
 
